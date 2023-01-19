66897b551991866f086da5670a5e36dbce9831bc
Giovani-Coelho <giovanicoelho2004@hotmail.com>
2023-01-19 11:51:59 -0300

    Base Project

---
+++ PrimeirJogo/Assets/Scrips/Player.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+  public float speed;
+  private Rigidbody2D rig;
+  public float jump;
+  public bool isJumping;
+  public bool doubleJumping;
+
+  private Animator anim;
+  void Start()
+  {
+    rig = GetComponent<Rigidbody2D>();
+    anim = GetComponent<Animator>();
+  }
+
+  void Update()
+  {
+    Move();
+    Jump();
+  }
+
+  void Move()
+  {
+    Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+    transform.position += movement * Time.deltaTime * speed;
+
+    if (Input.GetAxis("Horizontal") > 0f)
+    {
+      anim.SetBool("walk", true);
+      transform.eulerAngles = new Vector3(0f, 0f, 0f);
+    }
+
+    if (Input.GetAxis("Horizontal") < 0f)
+    {
+      anim.SetBool("walk", true);
+      transform.eulerAngles = new Vector3(0f, 180f, 0f);
+    }
+
+    if (Input.GetAxis("Horizontal") == 0f)
+    {
+      anim.SetBool("walk", false);
+    }
+  }
+  void Jump()
+  {
+    if (Input.GetButtonDown("Jump"))
+    {
+
+      if (!isJumping)
+      {
+        rig.AddForce(new Vector2(0f, jump), ForceMode2D.Impulse);
+        anim.SetBool("jump", true);
+        doubleJumping = true;
+      }
+
+      if (isJumping && doubleJumping)
+      {
+        rig.AddForce(new Vector2(0f, jump * 1.4f), ForceMode2D.Impulse);
+        anim.SetBool("doubleJump", true);
+        doubleJumping = false;
+      }
+    }
+  }
+
+  private void OnCollisionEnter2D(Collision2D collision)
+  {
+    if (collision.gameObject.layer == 6)
+    {
+      isJumping = false;
+      anim.SetBool("jump", false);
+      anim.SetBool("doubleJump", false);
+    }
+  }
+
+  private void OnCollisionExit2D(Collision2D collision)
+  {
+    if (collision.gameObject.layer == 6)
+    {
+      isJumping = true;
+    }
+  }
+}
